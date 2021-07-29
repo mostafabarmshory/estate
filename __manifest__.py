152c3494fd2919c073def8040a829cb8c6cd83cd
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2021-07-29 22:07:05 +0430

    Getting start to develop module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'EState',
+    'version': '0.1.0',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'The state module',
+    'description': 'This is a module of test',
+    'website': 'https://viraweb123.ir',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
