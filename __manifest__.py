c1664f30a6c1df31a39e355f5e83ec45337d2b29
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2021-07-30 21:57:58 +0430

    Adding property types

--- __manifest__.py
+++ __manifest__.py
@@ -13,6 +13,7 @@
         'security/estate_security.xml',
         'security/ir.model.access.csv',
 
+        'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ],
